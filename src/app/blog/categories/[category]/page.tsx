215c7488dd6b7405dd5df22d1fe2dc8df272df23
bazen <bazenatlas@gmail.com>
2023-11-12 19:53:27 -0500

    anything is possible

--- src/app/blog/categories/[category]/page.tsx
+++ src/app/blog/categories/[category]/page.tsx
@@ -8,7 +8,7 @@ import { groq } from 'next-sanity'
 const categoryTitlesQuery = groq`*[_type == "categories"].title`
 export async function generateStaticParams() {
   const titles = (await client.fetch(categoryTitlesQuery)) as string[]
-  return titles
+  return titles.map((title) => ({ category: title }))
 }
 async function getArticles(category: string) {
   const query = `*[_type == 'articles' && category->title == "${category}"]{
@@ -22,86 +22,38 @@ async function getArticles(category: string) {
   const data = await client.fetch(query)
   return data
 }
-
+export const dynamicParams = false
 export default async function CategoryPage({ params }: { params: any }) {
   const data: Article[] = await getArticles(params.category)
-
-  const titles = await client.fetch(categoryTitlesQuery)
-
   return (
     <Container>
       <h1 className='text-5xl first-letter:uppercase py-8'>
         {params.category}
       </h1>
-      {params.category && titles.includes(params.category) ? (
-        data.length > 0 ? (
-          <div className='grid grid-cols-3'>
-            {data.map((article, i) => (
-              <div
-                key={i}
-                className='px-6 py-8 border col-span-1'
-              >
-                <Image
-                  src={urlFor(article.teaserImage).url()}
-                  width={500}
-                  height={500}
-                  alt='change me'
-                  className='aspect-square object-cover'
-                />
-                <h1>{article.title}</h1>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className='pt-8'>
-            <p>No articles found in this category</p>
-          </div>
-        )
+
+      {data.length > 0 ? (
+        <div className='grid grid-cols-3'>
+          {data.map((article, i) => (
+            <div
+              key={i}
+              className='px-6 py-8 border col-span-1'
+            >
+              <Image
+                src={urlFor(article.teaserImage).url()}
+                width={500}
+                height={500}
+                alt='change me'
+                className='aspect-square object-cover'
+              />
+              <h1>{article.title}</h1>
+            </div>
+          ))}
+        </div>
       ) : (
-        notFound()
+        <div className='pt-8'>
+          <p>No articles found in this category</p>
+        </div>
       )}
     </Container>
   )
 }
-
-// export const articlesQuery = groq`*[_type == "articles" && category->title == defined(category)]{
-//     _id,
-//     title,
-//     "category": category->title,
-//       "teaserImage": teaserImage.asset->url,
-//       "slug": slug.current,
-// }`
-
-// async function getSlugs(slug: string) {
-//   const query = `*[_type == 'articles']{
-//       "slug": slug.current,
-//   }
-// `
-
-//   const data = await client.fetch(query)
-//   console.log('data', data)
-//   return data
-// }
-
-// // Return a list of `params` to populate the [slug] dynamic segment
-// export async function generateStaticParams(category: string) {
-//   const query = `*[_type == 'articles' && category->title == "${category}"]{
-//     _id,
-//     title,
-//     "category": category->title,
-//       "teaserImage": teaserImage.asset->url,
-//       "slug": slug.current,
-//   }
-// `
-//   const articles = await client.fetch(query)
-//   return articles.map((article: Article) => ({
-//     slug: article.slug,
-//   }))
-// }
-
-// Multiple versions of this page will be statically generated
-// using the `params` returned by `generateStaticParams`
-// export default function Page({ params }) {
-//   const { slug } = params
-//   // ...
-// }
