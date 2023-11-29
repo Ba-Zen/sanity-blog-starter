89e815b7e67e9451b9373a744d203b93d1e15df4
bazen <bazenatlas@gmail.com>
2023-11-29 14:50:03 -0500

    push

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '9a5s6sv0',
-    dataset: 'production'
-  }
+    projectId: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  },
 })
--- src/components/faqs.tsx
+++ src/components/faqs.tsx
@@ -11,13 +11,13 @@ export default function Faqs() {
     <Accordion type="single" collapsible className="">
       <AccordionItem value="item-1">
         <AccordionTrigger>
-          What's the best time to visit Boston?
+          What&apos;s the best time to visit Boston?
         </AccordionTrigger>
         <AccordionContent className="max-w-md">
           The best times to come to Boston are the Summer for the amount of
-          activities always going on. Boston is famous for it's Autumn season.
-          The fall is a great time to come to the area because the natural
-          beauty when the leaves turn brown.
+          activities always going on. Boston is famous for it&apos;s Autumn
+          season. The fall is a great time to come to the area because the
+          natural beauty when the leaves turn brown.
         </AccordionContent>
       </AccordionItem>
     </Accordion>
